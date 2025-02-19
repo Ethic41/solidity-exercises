f0328262ab0e763980848219fb1de1062ef1beb2
ethic41 <dahirmuhammad3@gmail.com>
2025-02-19 15:07:00 +0100

    solved FilterOddNumbers

--- FilterOddNumbers/src/FilterOddNumbers.sol
+++ FilterOddNumbers/src/FilterOddNumbers.sol
@@ -10,11 +10,23 @@ contract FilterOddNumbers {
               you need to count the even numbers then declare an array of that size.
     */
 
-    function filterOdd(uint256[] memory _arr)
-        public
-        view
-        returns (uint256[] memory)
-    {
-        // your code here
+    function filterOdd(uint256[] memory _arr) public view returns (uint256[] memory){
+        uint256[] memory evenNumbers = new uint256[](_arr.length);
+        uint256 count = 0;
+
+        for (uint256 i = 0; i < _arr.length; i++){
+            if (_arr[i] % 2 == 0){
+                evenNumbers[count] = _arr[i];
+                count += 1;
+            }
+        }
+
+        uint256[] memory result = new uint256[](count);
+
+        for (uint256 i = 0; i < count; i++){
+            result[i] = evenNumbers[i];
+        }
+
+        return result;
     }
 }
