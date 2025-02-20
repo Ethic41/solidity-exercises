8a598a4dc87df9b88842708b5e2899e5b6bfe8b2
ethic41 <dahirmuhammad3@gmail.com>
2025-02-20 17:56:42 +0100

    solved IsSorted

--- IsSorted/src/IsSorted.sol
+++ IsSorted/src/IsSorted.sol
@@ -5,7 +5,17 @@ contract IsSorted {
     /**
      * The goal of this exercise is to return true if the members of "arr" is sorted (in ascending order) or false if its not.
      */
-    function isSorted(uint256[] calldata arr) public view returns (bool) {
-        // your code here
+    function isSorted(uint256[] calldata arr) public pure returns (bool) {
+        uint256 lastNum = 0;
+
+        for(uint256 i = 0; i < arr.length; i++){
+            if (arr[i] < lastNum){
+                return false;
+            }
+
+            lastNum = arr[i];
+        }
+
+        return true;
     }
 }
